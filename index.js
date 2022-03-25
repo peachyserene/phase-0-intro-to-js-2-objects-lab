7bdb5d2bb267614321faa514078fcff11917c09c
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-03-24 22:55:29 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee ={
+    jared: "13 Milton St.",
+    emily: "452 South Bend Rd.",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+      };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const remainingEmployees = {...employee};
+    delete remainingEmployees[key];
+    return remainingEmployees;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
